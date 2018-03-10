e476a703c4cbcb3172fe34e943bfdd3dbe3b3600
Your Name <you@example.com>
2018-03-11 06:47:25 +0700

    Raymond Pulver, [11.03.18 06:46] Yes!
    Raymond Pulver, [11.03.18 06:46]
    this is the correct hash
    
    Raymond Pulver, [11.03.18 06:46]
    1) packing -- solved, 2) hashing -- solved, 3) signing -- still got an issue ��

--- xchange-idex/src/main/java/org/knowm/xchange/idex/IdexSignature.java
+++ xchange-idex/src/main/java/org/knowm/xchange/idex/IdexSignature.java
@@ -22,78 +22,82 @@ public class IdexSignature {
     static Boolean debugMe = IdexMarketDataService.Companion.getDebugMe();
 
     static/** Generate v, r, s values from payload */
-    SignatureData generateSignature(String apiSecret, List<List<String>> data) throws IOException {
-        ByteBuffer sig_arr = ByteBuffer.allocate(4096);
+    SignatureData generateSignature(String apiSecret, List<List<String>> data) {
+        byte[] rawhash = null;
+        byte[] saltBytes;
+        byte[] bytes = null;
+        final String[] last = new String[1];
+        BigInteger apiSecret1;
+        ECKeyPair ecKeyPair;
+        try (ByteArrayOutputStream sig_arr = new ByteArrayOutputStream()) {
 
-        if (debugMe) {
-            System.err.println(data);
+            if (debugMe) {
+                System.err.println(data);
+            }
+            for (List<String> d : data) {
+                String data1 = d.get(1);
+                /* remove 0x prefix and convert to bytes*/
+                if (debugMe) System.err.println("\n===\nsignature  for (len: " + d.get(1).length() + " ):" + d);
+                byte[] segment = new byte[0];
+                byte[] r = new byte[0];
+                last[0] = new LinkedList<>(asList(data1.toLowerCase().split("0x"))).getLast();
+                switch (d.get(2)) {
+                    case "address": {
+                        segment = new byte[20];
+                        r = new BigInteger(last[0], 16).toByteArray();
+
+                        break;
+                    }
+
+                    case "uint256": {
+                        segment = new byte[32];
+                        r = new BigInteger(last[0], 10).toByteArray();
+
+                        break;
+                    }
+
+                }
+                int segLen = segment.length;
+                int rlen = min(max(segLen, r.length), r.length);
+                int oversize = r.length - segLen;
+                System.arraycopy(r, oversize > 0 ? oversize : 0, segment, oversize > 0 ? 0 : (segLen - rlen), min(segLen, rlen));
+
+                sig_arr.write(segment);
+
+                if (debugMe) {
+                    System.err.println("signature: results: (len: " + rlen + "->" + segLen + ") " + Hex.encodeHexString(segment));
+                    System.err.println("signature: accumulated: " + Hex.encodeHexString(sig_arr.toByteArray()));
+                }
+            }
+            rawhash = sha3(sig_arr.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        data.stream().forEach(
-
-
-                (List<String> d) -> {
-                    String data1 = d.get(1);
-                    /* remove 0x prefix and convert to bytes*/
-                    if (debugMe) System.err.println("\n===\nsignature  for (len: " + d.get(1).length() + " ):" + d);
-                    byte[] segment = new byte[0];
-                    byte[] r = new byte[0];
-                    String last = new LinkedList<>(asList(data1.toLowerCase().split("0x"))).getLast();
-                    switch (d.get(2)) {
-                        case "address": {
-                            segment = new byte[20];
-                            r = new BigInteger(last, 16).toByteArray();
-
-                            break;
-                        }
-
-                        case "uint256": {
-                            segment = new byte[32];
-                            r = new BigInteger(last, 10).toByteArray();
-
-                            break;
-                        }
-
-                    }
-                    int segLen = segment.length;
-                    int rlen = min(max(segLen, r.length), r.length);
-                    int oversize = r.length - segLen;
-                    System.arraycopy(r, oversize > 0?oversize:0, segment, oversize > 0?0:(segLen - rlen), min(segLen,rlen));
-                    ByteBuffer snap = (ByteBuffer) sig_arr.duplicate().flip();
-                    sig_arr.put(segment);
-
-                    if (debugMe) {
-                        System.err.println("signature: results: (len: " + rlen+"->" +segLen + ") " + Hex.encodeHexString(segment));
-                        System.err.println("signature: accumulated: " + Hex.encodeHexString(buf2byte(sig_arr)));
-
-                    }
-                });
-
-        // hash the packed string
-        byte[] rawhash = new byte[0];
-
-        rawhash = sha3(buf2byte(sig_arr));
 
         // salt the hashed packed string
-        byte[] saltBytes = "\u0019Ethereum Signed Message:\n32".getBytes();
+        saltBytes = "\u0019Ethereum Signed Message:\n32".getBytes();
 
         assert (Hex.encodeHexString(
                 saltBytes).toLowerCase() == "19457468657265756d205369676e6564204d6573736167653a0a3332");
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        byteArrayOutputStream.write(saltBytes);
-        byteArrayOutputStream.write(rawhash);
-        byte[] bytes = byteArrayOutputStream.toByteArray();
-        byte[] salted = sha3(bytes);
+        byte[] salted=null;
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+            byteArrayOutputStream.write(saltBytes);
+            byteArrayOutputStream.write(rawhash);
+            bytes = byteArrayOutputStream.toByteArray();
+            salted = sha3(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         if (debugMe) {
             System.err.println("\n== unsalted:\t" + Hex.encodeHexString(rawhash));
             System.err.println("== salt raw :\t" + Hex.encodeHexString(saltBytes));
             System.err.println("== salted raw :\t" + Hex.encodeHexString(bytes));
             System.err.println("== salted hash:\t" + Hex.encodeHexString(salted));
         }
-        String last = new LinkedList<String>(asList(apiSecret.split("0x"))).getLast();
-        BigInteger apiSecret1 = new BigInteger(last, 16);
-
-        ECKeyPair ecKeyPair = ECKeyPair.create(apiSecret1);
+        last[0] = new LinkedList<String>(asList(apiSecret.split("0x"))).getLast();
+        apiSecret1 = new BigInteger(last[0], 16);
+        ecKeyPair = ECKeyPair.create(apiSecret1);
 
 
         return
@@ -103,7 +107,6 @@ public class IdexSignature {
                 );
     }
 
-    private static byte[] buf2byte(ByteBuffer sig_arr) {
-        return UTF_8.decode((ByteBuffer) ((ByteBuffer) sig_arr.duplicate()).flip()).toString().getBytes();
-    }
+
+
 }
