64149de07ff05070215366aa5dafc44de72d4bec
Your Name <you@example.com>
2018-04-06 09:44:34 +0700

    new rescu

--- xchange-idexjaxrs/src/main/java/org/knowm/xchange/idex/IdexExchange.java
+++ xchange-idexjaxrs/src/main/java/org/knowm/xchange/idex/IdexExchange.java
@@ -1,6 +1,7 @@
 package org.knowm.xchange.idex;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import org.knowm.xchange.BaseExchange;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
@@ -8,6 +9,7 @@ import org.knowm.xchange.ExchangeSpecification;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.marketdata.Ticker;
+import org.knowm.xchange.dto.marketdata.Ticker.Builder;
 import org.knowm.xchange.dto.marketdata.Trades;
 import org.knowm.xchange.idexjaxrs.dto.Market;
 import org.knowm.xchange.idexjaxrs.dto.ReturnTickerResponse;
@@ -16,11 +18,10 @@ import org.knowm.xchange.service.marketdata.MarketDataService;
 import si.mazi.rescu.RestProxyFactory;
 import si.mazi.rescu.SynchronizedValueFactory;
 
-public class IdexExchange extends BaseExchange implements Exchange {
+public class IdexExchange extends BaseExchange {
 
   @Override
-  protected void initServices() {
-  }
+  protected void initServices() {}
 
   @Override
   public SynchronizedValueFactory<Long> getNonceFactory() {
@@ -48,33 +49,46 @@ public class IdexExchange extends BaseExchange implements Exchange {
     };
   }
 
-
   @Override
   public MarketDataService getMarketDataService() {
     return new MarketDataService() {
       @Override
-      public Ticker getTicker(CurrencyPair currencyPair, Object... args) throws IOException {
+      public Ticker getTicker(CurrencyPair currencyPair, Object... args) {
 
-        ReturnTickerApi proxy = RestProxyFactory.createProxy(ReturnTickerApi.class, IdexExchange.this.exchangeSpecification.getSslUri());
+        ReturnTickerApi proxy =
+            RestProxyFactory.createProxy(ReturnTickerApi.class, exchangeSpecification.getSslUri());
         Ticker ret = null;
         try {
           Market market = new Market();
-          ReturnTickerResponse ticker = proxy.ticker(market.market(currencyPair.counter.getSymbol() + "_" + currencyPair.base.getSymbol()));
-          ret = new Ticker.Builder().currencyPair(currencyPair).build()/*incomplete*/;
+          ReturnTickerResponse ticker =
+              proxy.ticker(
+                  market.market(
+                      currencyPair.counter.getSymbol() + "_" + currencyPair.base.getSymbol()));
+
+          System.err.println(ticker.toString());
+          ret =
+              new Builder()
+                  .currencyPair(currencyPair)
+                  .last(new BigDecimal(ticker.getLast()))
+                  .ask(new BigDecimal(ticker.getLowestAsk()))
+                  .bid(new BigDecimal(ticker.getHighestBid()))
+                  .volume(new BigDecimal(ticker.getBaseVolume()))
+                  .high(new BigDecimal(ticker.getHigh()))
+                  .low(new BigDecimal(ticker.getLow()))
+                  .build() /*incomplete*/;
         } catch (Exception e) {
           e.printStackTrace();
         }
         return ret;
       }
 
-
       @Override
-      public OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) throws IOException {
+      public OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) {
         return null;
       }
 
       @Override
-      public Trades getTrades(CurrencyPair currencyPair, Object... args) throws IOException {
+      public Trades getTrades(CurrencyPair currencyPair, Object... args) {
         return null;
       }
     };
@@ -82,11 +96,7 @@ public class IdexExchange extends BaseExchange implements Exchange {
 
   public static void main(String[] args) throws IOException {
     Exchange exchange = ExchangeFactory.INSTANCE.createExchange(IdexExchange.class);
-    Ticker ticker = exchange.getMarketDataService().getTicker(new CurrencyPair(
-            "OMG",
-            "ETH"
-    ));
+    Ticker ticker = exchange.getMarketDataService().getTicker(new CurrencyPair("OMG", "ETH"));
     System.out.println(ticker);
-
   }
 }
